74d6692e331597941e10827ea0720a17a0514060
J.P. Krauss <jkrauss@asymworks.com>
2023-05-29 16:06:17 -0700

    Release 0.1.19

--- types/components/popupMenu.d.ts
+++ types/components/popupMenu.d.ts
@@ -3,12 +3,17 @@
  */
 export default class JtPopupMenu extends HTMLElement {
     _id: string;
+    _offset: number | null;
+    _placement: string;
+    _selector: string;
     _trigger?: HTMLElement;
     _target?: HTMLElement;
     /** @private */
-    _onFocusIn(ev: FocusEvent): void;
+    _onFocusIn(ev: Event): void;
     /** @private */
-    _onFocusOut(ev: FocusEvent): void;
+    _onFocusOut(ev: Event): void;
+    /** @private */
+    _position(): void;
     /** @private */
     _setup(): void;
     /**
@@ -20,6 +25,8 @@ export default class JtPopupMenu extends HTMLElement {
      */
     open(): void;
     constructor();
+    static get observedAttributes(): string[];
+    attributeChangedCallback(name: string): void;
     connectedCallback(): void;
     static register(): void;
 }
